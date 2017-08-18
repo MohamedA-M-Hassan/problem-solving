b6cc11af7655de78ac703ea77db2f2f03de82d70
boogy <anehmr0@gmail.com>
2017-08-18 19:00:04 +0200

    A841 done | at contest

---
+++ A841.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	//http://codeforces.com/problemset/problem/841/A
+	int arr[200] = {0};
+	string color;
+	int n, k;
+	cin >> n >> k >> color ;
+	bool flag = true;
+
+	for (int i = 0; i < n; i++)
+	{
+		arr[(int)color[i]]++;
+		if (arr[(int)color[i]] > k) { cout << "No"; flag = false; break; }
+	}
+	if (flag == true) cout << "YES";
+	return 0;
+}
